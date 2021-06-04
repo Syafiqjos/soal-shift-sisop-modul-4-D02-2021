ce64b0df17f3759e2316992037f397186c7ca986
Syafiqjos <syafiqjos@gmail.com>
2021-06-04 11:41:26 +0000

    Percobaan recursive

--- nomer1mod.c
+++ nomer1mod.c
@@ -17,9 +17,23 @@
 #include <dirent.h>
 
 static  const  char *dirpath = "/home/alecetra/Downloads";
+static const int buffer_size = 1024;
+
+char *get_dir_name(char *path){
+    char *filename = malloc(sizeof(char) * buffer_size);
+    int n = strlen(path);
+    int i = n - 1;
+    while (i >= 0){
+        if (path[i] == '/') break;
+        i--;
+    }
+    i++;
+    snprintf(filename, i, "%s", path);
+    return filename;
+}
 
 char *get_file_name(char *path){
-    char *filename = malloc(sizeof(char) * 256);
+    char *filename = malloc(sizeof(char) * buffer_size);
     int n = strlen(path);
     int i = n - 1;
     while (i >= 0){
@@ -32,7 +46,7 @@ char *get_file_name(char *path){
 }
 
 char *get_file_name_only(char *path){
-    char *filename = malloc(sizeof(char) * 256);
+    char *filename = malloc(sizeof(char) * buffer_size);
     char *temppath = get_file_name(path);
     int n = strlen(temppath);
     int i = 0;
@@ -47,7 +61,7 @@ char *get_file_name_only(char *path){
 }
 
 char *get_extension_name(char *path){
-    char *filename = malloc(sizeof(char) * 256);
+    char *filename = malloc(sizeof(char) * buffer_size);
     char *temppath = get_file_name(path);
     int n = strlen(temppath);
     int i = 0;
@@ -80,7 +94,7 @@ int get_lowercase_diff_decimal(char *path){
 }
 
 char *get_special_directory_name(char *path){
-    char *filename = malloc(sizeof(char) * 256);
+    char *filename = malloc(sizeof(char) * buffer_size);
     char *filename_only = get_file_name_only(path);
     char *extension_only = get_extension_name(path);
 
@@ -120,7 +134,7 @@ char *encrypt_atbash (char* str){
     int i=0;
     if(!strcmp(str,".") || !strcmp(str,"..")) return str;
     
-    char *output = malloc(1024);
+    char *output = malloc(buffer_size);
 
     while(str[i]!='\0')
     {
@@ -146,79 +160,101 @@ char *encrypt_atbash (char* str){
     	i++;
     }
 
+    output[i] = '\0';
+
     return output;
 }
 
-static  int  xmp_getattr(const char *path, struct stat *st)
-{
+int recursively_encode_atoz(char *fpath){
+    DIR *dir;
+    struct dirent *dp;
 
-    char fpath[2056];
-    char fnpath[2056];
+    dir = opendir(fpath);
+
+    if (dir == NULL) return -errno;
+
+    while ((dp = readdir(dir)) != NULL) {
+        if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0) {
+		    if(dp->d_type == DT_REG) {
+                char path1[buffer_size];
+
+                char old_name[buffer_size];
+                char *dir_name = get_dir_name(fpath);
+                char *file_name = get_file_name(dp->d_name);
+
+                sprintf(old_name, "%s/%s", dir_name, dp->d_name);
+                sprintf(path1, "%s/%s", dir_name, encrypt_atbash(file_name));
+                
+                rename(old_name, path1);
+
+                printf("Rename from : %s to %s\n", old_name, path1);
+            } else if(dp->d_type == DT_DIR) {
+                char path2[buffer_size];
+
+                char old_name[buffer_size];
+                char *dir_name = get_dir_name(fpath);
+                char *file_name = get_file_name(dp->d_name);
+
+                sprintf(old_name, "%s/%s", dir_name, dp->d_name);
+                sprintf(path2, "%s/%s", dir_name, encrypt_atbash(file_name));
+
+                rename(old_name, path2);
+
+                printf("Rename from : %s to %s\n", old_name, path2);
+
+                recursively_encode_atoz(path2);
+            }
+	     }
+    }
+
+    closedir(dir);
+    return 0;
+}
+
+void recursively_check_encoding(char *from, char* to){
+    char *toName = get_file_name(to);
+    char *fromName = get_file_name(from);
+
+    printf("REC : %s -> %s\n", fromName, toName);
+    if(strstr(fromName, "AtoZ_") == NULL && strstr(toName, "AtoZ_") == toName){
+        //encode atoz
+        printf("Initiate encode AtoZ\n");
+        recursively_encode_atoz(to);
+    } else if (strstr(fromName, "AtoZ_") == fromName && strstr(toName, "AtoZ_") == NULL){
+        //decode atoz
+        printf("Initiate decode AtoZ\n");
+        recursively_encode_atoz(to);
+    }
+}
+
+static int xmp_getattr(const char *path, struct stat *stbuf)
+{
+    char fpath[buffer_size];
 
     sprintf(fpath, "%s%s", dirpath, path);
 
     printf("GetAttr : %s\n", fpath);
 
-    char *filename = get_file_name(fpath);
-    char *encryptedfilename = filename;
+	int res;
 
-    if(strstr(filename, "AtoZ_") == filename)
-    {
-        encryptedfilename = encrypt_atbash(filename);
-    } else {
-	
-    }
+	res = lstat(fpath, stbuf);
+	if (res == -1)
+		return -errno;
 
-    sprintf(fnpath, "%s/%s", dirpath, encryptedfilename);
-
-    printf("GetAttrAtbash : %s\n", fnpath);
-
-    st->st_uid = getuid();
-    st->st_gid = getgid();
-    st->st_atime = time( NULL );
-    st->st_mtime = time( NULL );
-
-    if ( strcmp( path, "/" ) == 0 ) {
-	    	st->st_mode = S_IFDIR | 0755;
-		st->st_nlink = 2;
-    } else {
-	    	st->st_mode = S_IFREG | 0644;
-		st->st_nlink = 1;
-		st->st_size = 1024;
-    }
-
-    return 0;
+	return 0;
 }
-
-
 
 static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi)
 {
-    char fpath[1000];
-    //char name[1000];
-    int mode=0;
-    if(strstr(path, "AtoZ_")!=NULL)
-    {
-        mode=1;
-    }
+    char fpath[buffer_size];
 
-    if(strcmp(path,"/") == 0)
-    {
-        path=dirpath;
-        sprintf(fpath,"%s",path);
-    } else {
-        sprintf(fpath, "%s%s",dirpath,path);
-    }
+    sprintf(fpath, "%s%s", dirpath, path);
+    printf("ReadDir : %s\n", fpath);
 
     int res = 0;
 
     DIR *dp;
     struct dirent *de;
-    // (void) offset;
-    // (void) fi;
-    // char paths[100];
-   
-    // strcpy(paths, path);
 
     dp = opendir(fpath);
 
@@ -228,14 +264,10 @@ static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_
         struct stat st;
 
         memset(&st, 0, sizeof(st));
-        char temp[1000];
+        char temp[buffer_size];
         st.st_ino = de->d_ino;
         st.st_mode = de->d_type << 12;
         strcpy(temp, de->d_name);
-        if(mode==1)
-        {
-            encrypt_atbash(temp);
-        }
         res = (filler(buf, temp, &st, 0));
 
         if(res!=0) break;
@@ -246,26 +278,12 @@ static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_
     return 0;
 }
 
-
-
 static int xmp_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi)
 {
-    char fpath[1000];
-    if(strcmp(path,"/") == 0)
-    {
-        path=dirpath;
+    char fpath[buffer_size];
 
-        sprintf(fpath,"%s",path);
-    }
-    else {
-        char temp[1000];
-        strcpy(temp, path);
-
-        if(strstr(path, "/AtoZ_")!=NULL)
-            encrypt_atbash(temp);
-
-        sprintf(fpath, "%s%s",dirpath,temp);
-    }
+    sprintf(fpath, "%s%s", dirpath, path);
+    printf("ReadFile : %s\n", fpath);
 
     int res = 0;
     int fd = 0 ;
@@ -285,31 +303,153 @@ static int xmp_read(const char *path, char *buf, size_t size, off_t offset, stru
     return res;
 }
 
+static int xmp_mkdir(const char * path, mode_t mode){
+    char fpath[buffer_size];
+
+    sprintf(fpath, "%s%s", dirpath, path);
+    printf("MkDir : %s\n", fpath);
+
+    int res = mkdir(fpath, mode);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_rename(const char *from, const char *to)
+{
+    char fpath[buffer_size];
+    char tpath[buffer_size];
+
+    sprintf(fpath, "%s%s", dirpath, from);
+    sprintf(tpath, "%s%s", dirpath, to);
+
+    printf("RenameFrom : %s\n", fpath);
+    printf("RenameTo : %s\n", tpath);
+
+	int res;
+
+	res = rename(fpath, tpath);
+
+    recursively_check_encoding(fpath, tpath);
+
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_unlink(const char *path)
+{
+    char fpath[buffer_size];
+
+    sprintf(fpath, "%s%s", dirpath, path);
+    printf("Unlink : %s\n", fpath);
+
+	int res;
+
+	res = unlink(fpath);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
 static int xmp_rmdir(const char *path){
-    //printf("Enter => rmdir\n");
-    int res;
-    char fpath[1000] = "/home/alecetra/Downloads";
-    strcat(fpath, path);
-    char paths[100];
-    strcpy(paths, path);
-    res = rmdir(fpath);
+    char fpath[buffer_size];
+
+    sprintf(fpath, "%s%s", dirpath, path);
+    printf("RmDir : %s\n", fpath);
+    
+    int res = rmdir(fpath);
     if(res == -1){
         return -errno;
-    }else{
+    } else {
         printf("penulisan dan rmdir sukses\n");
-    }        
+    } 
     return 0;
 }
 
+static int xmp_write(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fi)
+{
+    char fpath[buffer_size];    
+
+    sprintf(fpath, "%s%s", dirpath, path);
+
+    printf("Write : %s\n", fpath);
+
+	int fd;
+	int res;
+
+	(void) fi;
+	fd = open(fpath, O_WRONLY);
+	if (fd == -1)
+		return -errno;
+
+	res = pwrite(fd, buf, size, offset);
+	if (res == -1)
+		res = -errno;
+
+	close(fd);
+	return res;
+}
+
+static int xmp_mknod(const char *path, mode_t mode, dev_t rdev)
+{
+    char fpath[buffer_size];    
+
+    sprintf(fpath, "%s%s", dirpath, path);
+
+    printf("MakeNod : %s\n", fpath);
+
+	int res;
+
+	/* On Linux this could just be 'mknod(fpath, mode, rdev)' but this
+	   is more portable */
+	if (S_ISREG(mode)) {
+		res = open(fpath, O_CREAT | O_EXCL | O_WRONLY, mode);
+		if (res >= 0)
+			res = close(res);
+	} else if (S_ISFIFO(mode))
+		res = mkfifo(fpath, mode);
+	else
+		res = mknod(fpath, mode, rdev);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_open(const char *path, struct fuse_file_info *fi)
+{
+    char fpath[buffer_size];    
+
+    sprintf(fpath, "%s%s", dirpath, path);
+
+    printf("Open : %s\n", fpath);
+
+	int res;
+
+	res = open(fpath, fi->flags);
+	if (res == -1)
+		return -errno;
+
+	close(res);
+	return 0;
+}
 
 static struct fuse_operations xmp_oper = {
     .getattr = xmp_getattr,
     .readdir = xmp_readdir,
+    .mkdir = xmp_mkdir,
+    .mknod = xmp_mknod,
+    .rename = xmp_rename,
+    .unlink = xmp_unlink,
     .rmdir = xmp_rmdir,
+    .open = xmp_open,
     .read = xmp_read,
+    .write = xmp_write
 };
-
-
 
 int  main(int  argc, char *argv[])
 {
